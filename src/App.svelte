d47cd2c7008003bc8bd17d4c20e755ed0eb54c24
Dakota <carmer95@gmail.com>
2024-07-12 15:06:25 -0400

    Initial Commit, Adding existing file structure to GitHub

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Boost from "./lib/Boost.svelte";
+  import { targetPlayer } from "./lib/Processor";
+  import { socketMessageStore } from "./lib/socket";
+</script>
+
+<main>
+    {#if $targetPlayer?.name}
+  <Boost percent="{$targetPlayer.boost}" />
+  {/if}
+
+  <!-- 
+  The {#if $targetPlayer?.name} does three things:
+
+  It always has the latest value contained by the targetPlayer Svelte store because we have added a $ to the front. This only works in .svelte files.
+  It checks $targetPlayer is not undefined (that’s what the ? before the . is doing)
+  It checks that whatever $targetPlayer is, it has a name property that is “truthy” (so not undefined or "") 
+  -->
+</main>
+
+<style>
+</style>
